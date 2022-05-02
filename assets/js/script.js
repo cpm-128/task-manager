70e1a1de4b94234ea198fe5469d12810c184f78c
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-02 17:55:57 -0400

    updated form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault(); //this prevents the browser from immediately refreshing
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-};
+    tasksToDoEl.appendChild(listItemEl);};
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
