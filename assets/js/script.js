70dd9d203cd9b7de53266fce6d50b9d63acb5272
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-03 10:12:04 -0400

    updated form field to store and display date in the list

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,10 @@ var formEl = document.querySelector("#task-form");
 
 var createTaskHandler = function(event) {
     event.preventDefault(); //this prevents the browser from immediately refreshing
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    listItemEl.textContent = taskNameInput;
     tasksToDoEl.appendChild(listItemEl);};
 
 formEl.addEventListener("submit", createTaskHandler);
